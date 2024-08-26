41c9faa8bb22ec380107491f1ab0c1c5d4e432ca
Venkata <venkataraju.bandaru8@gmail.com>
2024-08-26 18:46:29 +0530

    Passing read -s commmand

--- 05-variable.sh
+++ 05-variable.sh
@@ -3,3 +3,7 @@
 echo "Please enter your user name ::"
 read -s USERNAME
 echo "Entered username is : $USERNAME"
+
+echo "Enter your password"
+read -s PASSWORD
+echo "Entered password is : $PASSWORD"
