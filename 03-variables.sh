289416afd78fcb4acc31dda580c2547af6ad59ef
Venkata <venkataraju.bandaru8@gmail.com>
2024-08-26 18:19:39 +0530

    Varaibale was changed

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Raju
+PERSON2=Venkata
 
 echo "$PERSON1 :: Hi $PERSON2, How are you?"
 echo "{$PERSON2} :: Hello {$PERSON1}, I am fine. How are you doing?"
