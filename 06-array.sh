c251bb4e38b0d47a9b54e1364dba00e45a0729bd
Venkata <venkataraju.bandaru8@gmail.com>
2024-08-26 19:00:54 +0530

    array concept was passed

---
+++ 06-array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "KIWI" "ORANGE")
+echo "First fruit is : ${FRUITS[0]}"
+echo "Second fruit is : ${FRUITS[1]}"
+echo "Third fruit is : ${FRUITS[2]}"
+echo "All fruits are : ${FRUITS[@]}"
