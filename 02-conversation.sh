940b6802006293e66742626292697f3b291f48ed
Venkata <venkataraju.bandaru8@gmail.com>
2024-08-26 17:58:50 +0530

    I  added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh :: Hi Suresh, How are you?"
+echo "Suresh :: Hello Ramesh, I am fine. How are you doing?"
+echo "Ramesh :: I  am doing good.  Shat's going on"
+echo "Suresh :: I star learning Devops."
